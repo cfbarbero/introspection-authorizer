d1bc30a5030313c10cf3c59e39b2f158fd9616d9
crisb <cfbarbero@gmail.com>
2016-09-13 11:18:41 -0600

    Improved tooling

--- test/context.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    succeed: function(response) {
-        console.log('Succeed', response);
-    },
-    fail: function(response) {
-        console.log('Fail', response);
-    }
-};
